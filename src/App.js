2c02f851bbb75914f01e42ce7f479c7bb68a0da4
Tuliany <tuliany@mail.com>
2021-06-02 11:02:05 +0200

    working code

--- src/App.js
+++ src/App.js
@@ -8,19 +8,13 @@ export default function App() {
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
   const [forecast, setForecast] = useState([])
-  
+
   useEffect(() => {
     const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(function (position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
-
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-          setData(result)
-        });
       await fetch(`${process.env.REACT_APP_API_URL}/forecast?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
         .then(res => res.json())
         .then(result => {
@@ -33,23 +27,15 @@ export default function App() {
   return (
     <div className="App">
       <main>
-      {(typeof data.main != 'undefined') ? (
-        <>
-          <Weather weatherData={data} />
-        </>
-      ) : (
-        <div></div>
-      )}
-      {(typeof forecast.list != 'undefined') ? (
-        <>
-          <Forecast forecastData={forecast} />
-        </>
-      ) : (
-        <div></div>
-      )}
+        {(typeof forecast.list != 'undefined') ? (
+          <>
+            <Weather weatherData={forecast} />
+            <Forecast forecastData={forecast} />
+          </>
+        ) : (
+          <div></div>
+        )}
       </main>
     </div>
   );
 }
-
-
--- src/components/weather/index.js
+++ src/components/weather/index.js
@@ -2,13 +2,11 @@ import React, { useState, } from 'react';
 import './styles.css';
 import moment from 'moment';
 
-
 export const Weather = ({ weatherData }) => {
-  const [convert, setConvert] = useState(false)
-  const [unit, setUnit] = useState(`${weatherData.main.temp} C`)
-
-  const Farenheit = `${Math.round(weatherData.main.temp * 2 + 30)} F`
-  const Celcius = `${Math.round(weatherData.main.temp)} C`
+  const [convert, setConvert] = useState(true)
+  const Farenheit = `${Math.round(weatherData.list[0].main.temp * 2 + 30)} F`
+  const Celcius = `${Math.round(weatherData.list[0].main.temp)} C`
+  const [unit, setUnit] = useState(Celcius)
 
   const oppositeUnit = unit === Celcius ? "farenheit" : "celsius";
 
@@ -17,35 +15,33 @@ export const Weather = ({ weatherData }) => {
     convert ? setUnit(Celcius) : setUnit(Farenheit)
   }
 
-  const { icon } = weatherData.weather[0]
+  const { icon } = weatherData.list[0].weather[0]
 
   return (
     <>
       <main>
-        
         <section className="weather-info">
-          <header className="header" id="city-header">{weatherData.name} Weather</header>
+          <header className="header" id="city-header">{weatherData.list[0].name} Weather</header>
           <section className="weather-temp">
-            <img src={`${process.env.REACT_APP_ICON_URL}/${icon}@2x.png`} alt="weather-icon" height="200px" width="200px" />
+            <img src={`${process.env.REACT_APP_ICON_URL}/${icon}@2x.png`} alt="weather-icon" height="250px" width="250px" />
             <>
               <div>
                 <h1><i class="fa fa-thermometer-three-quarters" aria-hidden="true"></i> {unit}&deg; </h1>
-                <h2>  {weatherData.weather[0].main}</h2>
+                <h2 class="description">  {weatherData.list[0].weather[0].main}</h2>
               </div>
               <div className="sun-hours">
-                <h2><i class="fa fa-sun-o" title="sunrise" aria-hidden="true"></i> {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</h2>
-                <h2><i class="fa fa-moon-o" title="sunset" aria-hidden="true"></i> {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</h2>
+                <h2><i class="fa fa-sun-o" title="sunrise" aria-hidden="true"></i> {new Date(weatherData.city.sunrise * 1000).toLocaleTimeString('en-IN')}</h2>
+                <h2><i class="fa fa-moon-o" title="sunset" aria-hidden="true"></i> {new Date(weatherData.city.sunset * 1000).toLocaleTimeString('en-IN')}</h2>
               </div>
             </>
           </section>
           <section className="weather-etx">
-            <h3><i class="fa fa-tint" title="humidity" aria-hidden="true"></i> {weatherData.main.humidity} </h3>
-            <h3><i class="fas fa-wind" title="wind"></i> {weatherData.wind.speed} </h3>
+            <h3><i class="fa fa-tint" title="humidity" aria-hidden="true"></i> {weatherData.list[0].main.humidity} </h3>
+            <h3><i class="fas fa-wind" title="wind"></i> {weatherData.list[0].wind.speed} </h3>
             <h3><i class="fas fa-calendar-day" title="week day"></i> {moment().format('dddd')}</h3>
             <h3><i class="fas fa-calendar-alt" title="calender"></i> {moment().format('LL')}</h3>
           </section>
           <button className="convert" title={`convert to ${oppositeUnit}`} onClick={handleConvert}> {oppositeUnit}</button>
-
         </section>
       </main>
     </>
